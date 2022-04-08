c04b7387111fd072923c28b23c59571e116c727c
yujin <84204420+euphorxa@users.noreply.github.com>
2022-04-07 22:21:59 -0700

    Forcing gradle to build again before windows BSODs on me

--- discord/commands/build.gradle.kts
+++ discord/commands/build.gradle.kts
@@ -8,6 +8,7 @@ plugins {
 
 subprojects {
     dependencies{
+        implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.6.1")
         implementation("dev.kord:kord-core:0.8.0-M12")
         implementation("com.kotlindiscord.kord.extensions:kord-extensions:1.5.2-RC1")
     }
--- discord/database/build.gradle.kts
+++ discord/database/build.gradle.kts
@@ -2,3 +2,10 @@ plugins {
     kotlin("jvm")
     kotlin("plugin.serialization")
 }
+
+subprojects {
+    dependencies {
+        implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.6.1")
+        implementation("org.litote.kmongo:kmongo:4.5.1")
+    }
+}
