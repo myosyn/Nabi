ef7d90972d9ac021eafd15ba2ee14da77f99a8a6
yujin <84204420+euphorxa@users.noreply.github.com>
2022-04-17 12:33:05 -0700

    This should work

--- discord/core/build.gradle.kts
+++ discord/core/build.gradle.kts
@@ -4,4 +4,7 @@ plugins {
 
 dependencies {
     implementation(project(":discord:commands"))
+
+    implementation("dev.kord:kord-core:0.8.x-SNAPSHOT")
+    implementation("com.kotlindiscord.kord.extensions:kord-extensions:1.5.3-SNAPSHOT")
 }
