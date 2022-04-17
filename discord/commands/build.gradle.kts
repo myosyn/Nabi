adef7ddb900650071d6eb6425a8c4fb36f1ab449
vispin <92119905+vispin@users.noreply.github.com>
2022-04-17 09:39:27 -0700

    I BUMPED THE WRONG BRANCH LOL

--- discord/commands/build.gradle.kts
+++ discord/commands/build.gradle.kts
@@ -4,6 +4,6 @@ plugins {
 
 dependencies {
     implementation("dev.kord:kord-core:0.8.x-SNAPSHOT")
-    implementation("com.kotlindiscord.kord.extensions:kord-extensions:1.5.2-SNAPSHOT")
+    implementation("com.kotlindiscord.kord.extensions:kord-extensions:1.5.3-SNAPSHOT")
 
 }
