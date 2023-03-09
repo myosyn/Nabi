86c492735c966620c067968d230a66f4b3f0a951
yujin <84204420+euphorxa@users.noreply.github.com>
2023-03-08 23:14:44 -0800

    Really
    Signed-off-by: yujin <84204420+euphorxa@users.noreply.github.com>

--- build.gradle.kts
+++ build.gradle.kts
@@ -15,5 +15,6 @@ allprojects {
         mavenLocal()
         maven("https://jitpack.io")
         maven("https://oss.sonatype.org/content/repositories/snapshots")
+        maven("https://s01.oss.sonatype.org/content/repositories/snapshots")
     }
 }
