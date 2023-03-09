6d7660a5062883743f998ad1f1e80fb288915f24
yujin <84204420+euphorxa@users.noreply.github.com>
2023-03-08 23:10:09 -0800

    I forgot to delete this repository since the maven repo for kordex is down now and on sonatype
    Signed-off-by: yujin <84204420+euphorxa@users.noreply.github.com>

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,8 +1,8 @@
 plugins {
-    kotlin("jvm") version "1.7.21" apply false
-    kotlin("plugin.serialization") version "1.7.21" apply false
-    id("org.jetbrains.dokka") version "1.7.20"
-    id("com.github.johnrengelman.shadow") version "7.1.2" apply false
+    kotlin("jvm") version "1.8.10" apply false
+    kotlin("plugin.serialization") version "1.8.10" apply false
+    id("org.jetbrains.dokka") version "1.8.10"
+    id("com.github.johnrengelman.shadow") version "8.1.0" apply false
     application
     java
 }
@@ -15,6 +15,5 @@ allprojects {
         mavenLocal()
         maven("https://jitpack.io")
         maven("https://oss.sonatype.org/content/repositories/snapshots")
-        maven("https://maven.kotlindiscord.com/repository/maven-public/")
     }
 }
