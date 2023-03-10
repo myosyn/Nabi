3a67b40c3aa0ff628e921af530596ffa1482c5c6
yujin <84204420+shuuyu@users.noreply.github.com>
2023-03-09 22:21:43 -0800

    please
    Signed-off-by: yujin <84204420+euphorxa@users.noreply.github.com>

--- bot/valement/src/main/kotlin/live/shuuyu/valement/commands/AbstractApplicationCommandBuilder.kt
+++ bot/valement/src/main/kotlin/live/shuuyu/valement/commands/AbstractApplicationCommandBuilder.kt
@@ -1,9 +1,7 @@
 package live.shuuyu.valement.commands
 
-import live.shuuyu.valement.annotations.PleaseSendHelp
 import live.shuuyu.valement.commands.options.SlashCommandArgument
 
-@PleaseSendHelp
 abstract class AbstractSlashCommand {
     abstract suspend fun execute(ctx: ApplicationCommandContext, args: SlashCommandArgument)
 }
