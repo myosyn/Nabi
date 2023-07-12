afab2c79fed36f694d44eeb8f78bd5e75f3152d5
shuuy <84204420+shuuyu@users.noreply.github.com>
2023-07-11 18:15:43 -0700

    I forgot to push color utils

---
+++ bot/discord-kord/src/main/kotlin/live/shuuyu/nabi/kord/utils/ColorUtils.kt
@@ -0,0 +1,9 @@
+package live.shuuyu.nabi.kord.utils
+
+import dev.kord.common.Color
+
+object ColorUtils {
+    val SUCCESS_COLOR = Color(230, 140, 255)
+    val FAIL_COLOR = Color(255, 140, 155)
+    val DEFAULT_COLOR = Color(250, 250, 250)
+}
